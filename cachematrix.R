1b68bae5cd21c1f79d2e939ced306b5c62b6f775
harshalathawale <harshal.athawale@gmail.com>
2016-07-24 14:57:13 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## x: a square invertible matrix
+  ## VVIMP : return: a list containing functions to Set & get the matrix and set & get the inverse
+  ##  VVIMP : this list is used as the input to cacheSolve()
+  
+  inv = NULL
+  set = function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## VVIMP : x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
+  
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    # get it from the cache and skips the computation. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  
+  return(inv)
 }
+  ## Example : Taken 9 normal Random Variables 
+n <- matrix(rnorm(9),3,3)
+  ## prints the value of n
+n
+  ## h  return: a list containing functions to Set & get the matrix and set & get the inverse
+  ## this list is used as the input to cacheSolve()
+h <- makeCacheMatrix(n)
+  ## returns inverse
+cacheSolve(h)
